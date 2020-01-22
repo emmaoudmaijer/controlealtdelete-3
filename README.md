d4e8a0bb206a826daaf5744d0840d8394c3537e6
emmaoudmaijer <emma.oudmaijer@hva.nl>
2020-01-22 15:09:12 +0100

    change readme

--- README.md
+++ README.md
@@ -5,9 +5,12 @@ Tijdens het project voor information design hebben we een datavisualisatie gemaa
 
 ![foto](public/images/bubblechart.png)
 
-### Het concept:
+## Wie is onze opdrachtgever: Controle Alt Delete:
 
 Door de Hogeschool van Amsterdam is ons een opdrachtgever toegewezen, Controle Alt Delete. Dit is een organisatie die zich focust op etnisch profileren bij de politie. Ze zetten zich in tegen etnisch profileren. Amnesty International is een samenwerkings- partner van Controle Alt Delete. Ze richten zich echter niet alleen op de politie maar ook op bijvoorbeeld BOA’s en de Koninklijke Marechaussee. Met dit project hebben we alleen met de politie te maken. Easy Solutions is een onderneming waar Controle Alt Delete een project van is. Ze verstoppen dit omdat Easy Solutions meerdere opdrachtgevers heeft die dit een heel spannend/politiek onderwerp kunnen vinden (denk aan de gemeente, etc.). Controle Alt Delete wordt als organisatie gepresenteerd, terwijl het eigenlijk een project is. Het is verder geregistreerd als een B.V. dat geen winst maakt. Wij burgers weten niet wat de omvang van etnisch profileren is. Daardoor kunnen wij niet meten of de maatregelen die de politie neemt het gewenste effect hebben.
+
+## Het concept:
+Ons concept laat verschillende variabelen zien zoals het aantal keer contact tussen de politie en de Amsterdamse respondenten, de manier van het contact en het vertrouwen in deze contact in verhouding met de culturele achtergrond van deze respondenten.
 
 ### Doelstelling
 De doelstelling is om door middel van onafhankelijke onderzoek cijfers te verzamelen die sterke indicaties geven voor de omvang van etnisch profileren in Nederland.
@@ -17,9 +20,22 @@ op de huisstijl. Wij willen met dit project een verhaal vertellen en in procente
 ### Doelgroep
 Onze website is bedoeld voor de orginatisoren van controle alt delete, maar ook voor mensen die vinden dat ze onterecht zijn behandeld door de politie en die via controle alt delete op deze pagina terecht zijn gekomen. Zo krijgen de bezoekers van de website een indicatie van hoe het contact met de politie tot stand komt en wat voor effect dit heeft met de culturele afkomsten. Ook is de website bedoeld voor de politie om hun ook een indicatie te geven van hoe het contact is verdeeld.
 
-## De visualisaties
+### De visualisaties
 
+In totaal zijn er drie datavisualisaties te zien op deze pagina. Deze hebben te maken met de **culturele achtergronden** van de respondenten, welke van deze respondenten er **contact** hebben gehad met de politie en wie van hen er **op de politie is afgestapt of andersom**, hoe het **contact** tot stand is gekomen en hoeveel **vertrouwen** de respondenten hebben in de politie. 
 
+**De bubble chart die je als eerst ziet:** In totaal zijn er 1934 respondenten geweest met verschillende culturele afkomsten, hiervan zijn er 794 in contact geweest met de politie in de afgelopen 12 maanden. Nederlandse Nederlanders het meest in contact geweest met de politie, gevolgd door overige Nederlanders met een niet Westerse migratieachtergrond en Surinamers. Je zou hieruit kunnen opmaken dat de kans aanwezig is dat de politie etnisch profileert. Alles in de bubble chart hebben we in procenten berekend zodat de verhoudingen dan eerlijker zijn en beter te vergelijken.
+
+**De bar chart die je als tweede ziet:**
+Bij deze bar chart hebben we gekeken naar hoe het contact met de politie en de respondent tot stand is gekomen. Zo is er te zien dat alleen Nederlanders met een Westerse migratieachtergrond en Nederlandse Nederlanders over het algemeen vaker zelf op de politie stappen en dat 
+de rest van de culturele afkomsten vaker worden aangesproken door de politie.
+
+![bar chart](images/visualisatie2.png)
+
+**De scatterplot die je als onderaan ziet:**
+De grafiek onderaan de one pager is een scatterplot. Hieruit kun je aflezen wat de totstandkoming van het contact te maken heeft met het vertrouwen van de respondent. Je ziet twee losse grafieken. De bovenste is voor wanneer de politie naar de respondent zelf is toegestapt en de tweede is voor wanneer de respondent zelf naar de politie is gestapt. De cijfers onderaan staan voor het aantal vertrouwen dat de respondent in de politie heeft en de transparantie staat voor het aantal procent (met culturele achtergrond) die dat cijfer heeft gegeven. Op deze manier kan je zien dat iemand die zelf op de politie stapt over het algemeen meer vertrouwen heeft in de politie dan andersom.
+
+![scatterplot](images/scatterplot.png)
 
 
 ## Data 
@@ -37,8 +53,7 @@ function dataOmzet() {
 					afkomst: result.Herkomst_def,
 					totstand: result.Totstand,
 					contact: result.Contact_gehad,
-					freqcontact: result.freqcontact,
-					cijfer: result.rapportcijfer
+					freqcontact: result.freqcontact
 				}
 			})
 			bubbleChart(newResults)
@@ -46,6 +61,7 @@ function dataOmzet() {
 }
 dataOmzet()
 ```
+Uit de data is hier de culturele afkomst, de totstandkoming, en het aantal keer contact hier opgehaald. Dit moet natuurlijk we 
 
 ## Install project
 Clone deze repository naar je computer:
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -17,21 +17,6 @@ function dataOmzet() {
 }
 dataOmzet()
 
-// function removeInvalidRecords(dataset) {
-// 	var dataset_clean = [];
-
-// 	for (var i = 0; i < dataset.length; i++) {
-// 		var obj = dataset[i];
-// 		if (obj.freqcontact != '99999' && obj.afkomst != 'Onbekend' && obj.afkomst != '#NULL!' && obj.afkomst != undefined) {
-
-// 			dataset_clean.push(JSON.parse(JSON.stringify(dataset[i])));
-// 			//total = total + obj.freqcontact;
-// 		}
-// 	}
-
-// 	return dataset_clean;
-// }
-//data = removeInvalidRecords(dataset);
 
 function removeInvalidRecords(dataset) {
 	var dataset_clean = [];
@@ -58,21 +43,6 @@ function getToMeData(data) {
 function bubbleChart(results) {
 
 		//-------------- DATASET 1: ALGEMEEN CONTACT
-
-// function removeInvalidRecords(results) {
-// 	var dataset_clean = [];
-
-// 	for (var i = 0; i < dataset.length; i++) {
-// 		var obj = results[i];
-// 		if (obj.freqcontact != '99999' && obj.afkomst != 'Onbekend' && obj.afkomst != '#NULL!' && obj.afkomst != undefined) {
-
-// 			dataset_clean.push(JSON.parse(JSON.stringify(result[i])));
-// 			//total = total + obj.freqcontact;
-// 		}
-// 	}
-
-// 	return dataset_clean;
-//}
 		 data = removeInvalidRecords(results);
 		 
 	
@@ -126,8 +96,6 @@ function bubbleChart(results) {
 
 	newDataRaw = newDataRaw.flat()
 
-	//var newDataRaw = removeInvalidRecords(newDataOrg);
-
 	var total2 = newDataRaw.reduce(function (accumulator, currentValue) {
 		return accumulator + currentValue.value
 	}, 0);
@@ -153,6 +121,17 @@ function bubbleChart(results) {
 		.entries(naarMij)
 
 	newDataRaw2 = newDataRaw2.flat()
+
+	var total2 = newDataRaw2.reduce(function (accumulator, currentValue) {
+		return accumulator + currentValue.value
+	}, 0);
+	//console.log("total2 :", total2);
+
+	for (var i = 0; i < newDataRaw2.length; i++) {
+		var obj2 = newDataRaw2[i];
+		newDataRaw2[i].value = Math.round((obj2.value / total2) * 100, 0);
+	}
+
 	let newDataRawTemp2 = JSON.stringify(newDataRaw2);
 	newData2 = {
 		children: JSON.parse(newDataRawTemp2)
@@ -175,8 +154,6 @@ function bubbleChart(results) {
 	var diameter = 600;
 
 	var color = d3.scaleOrdinal(d3.schemeCategory20);
-
-	color.range(['red', 'yellow', 'green', 'blue', 'orange']);
 
 	var bubble = d3.pack(dataset)
 		.size([diameter, diameter])
@@ -237,12 +214,9 @@ function bubbleChart(results) {
 			setFontSizeLegenda(d.data.key, 'OUT');
 		});
 
-	//console.log(node);
-
 	node.append("title")
 		.text(function (d) {
 			return d.key + ": " + d.value;
-			//return d.value;	
 		});
 
 	    node.append("circle")
@@ -302,6 +276,21 @@ function update2() {
 		.attr("class", "node")
 		.attr("transform", function (d) {
 			return "translate(" + d.x + "," + d.y + ")";
+		})
+		.on("mouseover", function (d) {
+			div.transition()
+				.duration(200)
+				.style("opacity", .9);
+			div.html(d.data.key + "<br/>" + d.data.value)
+				.style("left", (d3.event.pageX) + "px")
+				.style("top", (d3.event.pageY - 28) + "px");
+			setFontSizeLegenda(d.data.key, 'ON');
+		})
+		.on("mouseout", function (d) {
+			div.transition()
+				.duration(500)
+				.style("opacity", 0);
+			setFontSizeLegenda(d.data.key, 'OUT');
 		});
 
 	node.append("circle")
@@ -335,7 +324,7 @@ function update2() {
 		.style("fill", "white")
 		.text(function (d) {
 			return d.data.value + "% ";
-		});
+		})
 
 	}
 
@@ -359,6 +348,21 @@ function update2() {
 			.attr("class", "node")
 			.attr("transform", function (d) {
 				return "translate(" + d.x + "," + d.y + ")";
+			})
+			.on("mouseover", function (d) {
+				div.transition()
+					.duration(200)
+					.style("opacity", .9);
+				div.html(d.data.key + "<br/>" + d.data.value)
+					.style("left", (d3.event.pageX) + "px")
+					.style("top", (d3.event.pageY - 28) + "px");
+				setFontSizeLegenda(d.data.key, 'ON');
+			})
+			.on("mouseout", function (d) {
+				div.transition()
+					.duration(500)
+					.style("opacity", 0);
+				setFontSizeLegenda(d.data.key, 'OUT');
 			});
 	
 		node.append("circle")
@@ -415,205 +419,35 @@ function update2() {
 
 
 }
-//EINDE VAN BUBBLE CHART
+//---------------------------------------------- EINDE VAN BUBBLE CHART ------------------------------------------------------
 
+// (function ($) {
+// 	"use strict";
 
+// 	/* Navbar Scripts */
+// 	// jQuery to collapse the navbar on scroll
+// 	$(window).on('scroll load', function () {
+// 		if ($(".navbar").offset().top > 20) {
+// 			$(".fixed-top").addClass("top-nav-collapse");
+// 		} else {
+// 			$(".fixed-top").removeClass("top-nav-collapse");
+// 		}
+// 	});
 
+// 	// jQuery for page scrolling feature - requires jQuery Easing plugin
+// 	$(function () {
+// 		$(document).on('click', 'a.page-scroll', function (event) {
+// 			var $anchor = $(this);
+// 			$('html, body').stop().animate({
+// 				scrollTop: $($anchor.attr('href')).offset().top
+// 			}, 600, 'easeInOutExpo');
+// 			event.preventDefault();
+// 		});
+// 	});
 
-(function ($) {
-	"use strict";
-
-	/* Navbar Scripts */
-	// jQuery to collapse the navbar on scroll
-	$(window).on('scroll load', function () {
-		if ($(".navbar").offset().top > 20) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
-	});
-
-	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function () {
-		$(document).on('click', 'a.page-scroll', function (event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 600, 'easeInOutExpo');
-			event.preventDefault();
-		});
-	});
-
-	// closes the responsive menu on menu item click
-	$(".navbar-nav li a").on("click", function (event) {
-		if (!$(this).parent().hasClass('dropdown'))
-			$(".navbar-collapse").collapse('hide');
-	});
-})
-
-
-// //-------SCATTERPLOT DOOR LAURENS-------
-// const width = 600
-// const height = 600
-
-// let svg = d3.select("#chart3")
-//   .append("svg")
-
-// //const svg = d3.select('svg')
-// //console.log(svg)
-// svg.attr('width', width + "px")
-// svg.attr('height', height + "px")
-// const x = d3.scaleLinear()
-// 	.domain([0, 10])
-// 	.range([0, width]);
-// const y = d3.scaleLinear()
-// 	.domain(["Ik ging naar de politie", "De politie kwam naar mij"])         // This is what is written on the Axis: from 0 to 100
-// 	.range([ height, 0]);
-
-// const color = d3.scaleOrdinal(d3.schemeDark2)
-
-// //d3.json(dataSource)
-// let dataSource = fetch('../convertcsvdata.json') 
-// 	.then(data => data.json())
-// 	.then(json => {
-//   	console.log("starting with data length", json.length)
-//   	const filtered = json.filter(item => checkInvalid(item))
-//     console.log("data length after filtering 99999", filtered.length)
-//     const data = filtered.map(result => {
-//       return {
-//         id: result.response_ID,
-//         afkomst: result.Herkomst_def,
-//         totstand: result.Totstand == "De politie kwam naar mij toe" ? 1 : 0,
-//         contact: result.Contact_gehad,
-//         freqcontact: result.freqcontact,
-//         rapportcijfer: result.rapportcijfer
-//     	}
-// 		})
-//     const transformed = transformData(data)
-//    // console.log(transformed)
-
-//   	addScales(transformed)
-//   	drawScatterPlot(transformed)
-//   })
-
-// function transformData(data){
-// 	const transformed = d3.nest()
-//   	//first group all respondents by afkomst
-//     .key(d => d.afkomst)
-//     .rollup(d => {
-//       return {
-//       	amount: d.length,
-//         benaderdTotaal: d.filter(respondent => respondent.totstand == 1).length,
-//         nietBenaderdTotaal: d.filter(respondent => respondent.totstand == 0).length,
-// 				//Create an array of 0s for each rapportcijfer percentage we will fill in later
-//         rapportCijfersBenaderd: [0,0,0,0,0,0,0,0,0,0],
-//         rapportCijfersNietBenaderd: [0,0,0,0,0,0,0,0,0,0],
-//       }
-//   	})
-//   	.entries(data)
-//   //For each respondent, check if they approached the police and increment the amount of respondents that
-//   // had a certain rapportcijfer
-//   data.forEach(respondent => {
-//   	const land = transformed.find(afkomst => afkomst.key == respondent.afkomst)
-//     //console.log(land)
-//     if (respondent.totstand == 1){
-//       land.value.rapportCijfersBenaderd[respondent.rapportcijfer - 1] ++
-//     } else if (respondent.totstand == 0){
-//       land.value.rapportCijfersNietBenaderd[respondent.rapportcijfer - 1] ++
-//     }		
-//   })
-//   //For each country, convert the amount of people that had a certain rapportcijfer to a percentage
-//   transformed.forEach(land => {
-//     // console.log(land.value.rapportCijfersBenaderd)
-//     land.value.rapportCijfersBenaderd.forEach( (cijfer, index, array) => {
-//       array[index] = Math.round((cijfer / land.value.benaderdTotaal) * 100)
-//     })
-//     land.value.rapportCijfersNietBenaderd.forEach( (cijfer, index, array) => {
-//       array[index] = Math.round((cijfer / land.value.nietBenaderdTotaal) * 100)
-//     })
-//   })
-//   return transformed
-// }
-
-// //This function removes any items that have 99999 as an answer to any of the questions we're using for this viz!
-// function checkInvalid(item){
-//   const importantKeys = ["Herkomst_def", "Totstand", "Contact_gehad", "freqcontact" ,"rapportcijfer"]
-// 	for (let key of importantKeys) {
-//   	if (item[key] == '99999') {
-//       // console.log("removing invalid item because", key)
-//     	return false
-//     }
-//   }
-//   return true
-// }
-// function drawScatterPlot(data){  
-//   data.forEach( (country, index) => {
-//     //console.log(country)
-//     //Add all the circles for approached by police
-//   	let circlesTop = svg.selectAll(".top")
-//       .data(country.value.rapportCijfersBenaderd)
-//       .enter()
-//       .append("circle")
-//         .attr("class", country)
-//         .attr("r", d => circleSize(d))
-//         .attr("cx", (d, i) => x(i + 1))
-//         .attr("cy", (10 * index) + 100)
-// 			.attr("fill", color(index))
-// 		// .on("mouseover", function(d) {
-// 		// 	div.transition()
-// 		// 		.duration(200)
-// 		// 		.style("opacity", .9);
-// 		// 	div.html(d.data.key + "<br/>" + d.data.value)
-// 		// 		.style("left", (d3.event.pageX) + "px")
-// 		// 		.style("top", (d3.event.pageY - 28) + "px")
-// 		// 	})
-// 		// .on("mouseout", function(d) {
-// 		// 	div.transition()
-// 		// 		.duration(500)
-// 		// 		.style("opacity", 0);
-// 		// 	});
-
-//     //Add all the circles for approached police themselves
-//     let circlesBottom = svg.selectAll(".bottom")
-//       .data(country.value.rapportCijfersNietBenaderd)
-//       .enter()
-//       .append("circle")
-//         .attr("class", country)
-//         .attr("r", d => circleSize(d))
-//         .attr("cx", (d, i) => x(i + 1))
-//         .attr("cy", (10 * index) + 300)
-//     		.attr("fill", color(index))
-//   })
-// }
-
-// function addScales(data){
-//  	x.domain([1,10])	//From lowest to highest cijfer
-//   	.range([0, width])
-//   y.domain([0,255])
-//   	//By putting height first, we're fixing the inverse effect of y
-//   	.range([height,0])
-// }
-
-// //Determine the circlesize. I've tweaked this by hand  but there are nicer ways
-// function circleSize(item){
-// 	if (item == 0){
-//    return 0.5
-//   }
-//   else if (item < 5){
-//    return 2 
-//   }
-//   else if (item < 15){
-//    return 3 
-//   }
-//   else if (item < 25){
-//    return 4 
-//   }
-//   else if (item < 35){
-//    return 5 
-//   }
-//   else if (item < 45){
-//    return 6 
-//   }
-//   else return 100
-// }
-// //}());
+// 	// closes the responsive menu on menu item click
+// 	$(".navbar-nav li a").on("click", function (event) {
+// 		if (!$(this).parent().hasClass('dropdown'))
+// 			$(".navbar-collapse").collapse('hide');
+// 	});
+// })
